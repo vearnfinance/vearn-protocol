1fe2d5b03b630ab634680caa2e423a25bdc007e9
VFI <71346651+VearnFinance@users.noreply.github.com>
2020-09-16 13:43:20 -0700

    Update and rename interfaces/yearn/Token.sol to interfaces/vearn/Token.sol

--- interfaces/vearn/Token.sol
+++ interfaces/vearn/Token.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.5.17;
 
 // NOTE: Basically an alias for Vaults
-interface yERC20 {
+interface vERC20 {
   function deposit(uint256 _amount) external;
   function withdraw(uint256 _amount) external;
   function getPricePerFullShare() external view returns (uint);
