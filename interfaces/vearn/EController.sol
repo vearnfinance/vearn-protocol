9f24bbe56622c888d4ee1b8d31e9e5dd0b3b71bf
VFI <71346651+VearnFinance@users.noreply.github.com>
2020-09-16 13:44:50 -0700

    Update and rename interfaces/yearn/IController.sol to interfaces/vearn/EController.sol

--- interfaces/vearn/EController.sol
+++ interfaces/vearn/EController.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.16;
 
-interface IController {
+interface EController {
     function withdraw(address, uint) external;
     function balanceOf(address) external view returns (uint);
     function earn(address, uint) external;
