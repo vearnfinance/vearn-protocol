c6f61cb5d2dca16a47d29cfaa40391c10c54708d
VFI <71346651+VearnFinance@users.noreply.github.com>
2020-09-16 17:19:41 -0700

    Update StrategyTUSDCurve.sol

--- contracts/strategies/StrategyTUSDCurve.sol
+++ contracts/strategies/StrategyTUSDCurve.sol
@@ -10,9 +10,9 @@ import "@openzeppelinV2/contracts/token/ERC20/SafeERC20.sol";
 import "../../interfaces/curve/Curve.sol";
 import "../../interfaces/uniswap/Uni.sol";
 
-import "../../interfaces/yearn/IController.sol";
-import "../../interfaces/yearn/Mintr.sol";
-import "../../interfaces/yearn/Token.sol";
+import "../../interfaces/vearn/EController.sol";
+import "../../interfaces/vearn/Mintr.sol";
+import "../../interfaces/vearn/Token.sol";
 
 contract StrategyTUSDCurve {
     using SafeERC20 for IERC20;
@@ -20,22 +20,22 @@ contract StrategyTUSDCurve {
     using SafeMath for uint256;
     
     address constant public want = address(0x0000000000085d4780B73119b644AE5ecd22b376);
-    address constant public y = address(0x73a052500105205d34Daf004eAb301916DA8190f);
-    address constant public ycrv = address(0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8);
-    address constant public yycrv = address(0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c);
+    address constant public v = address(0x73a052500105205d34Daf004eAb301916DA8190f);
+    address constant public vcrv = address(0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8);
+    address constant public vvcrv = address(0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c);
     address constant public curve = address(0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51);
     
     address constant public dai = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    address constant public ydai = address(0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01);
+    address constant public vdai = address(0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01);
 
     address constant public usdc = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    address constant public yusdc = address(0xd6aD7a6750A7593E092a9B218d66C0A814a3436e);
+    address constant public vusdc = address(0xd6aD7a6750A7593E092a9B218d66C0A814a3436e);
 
     address constant public usdt = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
-    address constant public yusdt = address(0x83f798e925BcD4017Eb265844FDDAbb448f1707D);
+    address constant public vusdt = address(0x83f798e925BcD4017Eb265844FDDAbb448f1707D);
 
     address constant public tusd = address(0x0000000000085d4780B73119b644AE5ecd22b376);
-    address constant public ytusd = address(0x73a052500105205d34Daf004eAb301916DA8190f);
+    address constant public vtusd = address(0x73a052500105205d34Daf004eAb301916DA8190f);
 
     
     address public governance;
@@ -53,21 +53,21 @@ contract StrategyTUSDCurve {
     function deposit() public {
         uint _want = IERC20(want).balanceOf(address(this));
         if (_want > 0) {
-            IERC20(want).safeApprove(y, 0);
-            IERC20(want).safeApprove(y, _want);
-            yERC20(y).deposit(_want);
+            IERC20(want).safeApprove(v, 0);
+            IERC20(want).safeApprove(v, _want);
+            vERC20(v).deposit(_want);
         }
-        uint _y = IERC20(y).balanceOf(address(this));
-        if (_y > 0) {
-            IERC20(y).safeApprove(curve, 0);
-            IERC20(y).safeApprove(curve, _y);
-            ICurveFi(curve).add_liquidity([0,0,0,_y],0);
+        uint _v = IERC20(y).balanceOf(address(this));
+        if (_v > 0) {
+            IERC20(v).safeApprove(curve, 0);
+            IERC20(v).safeApprove(curve, _v);
+            ICurveFi(curve).add_liquidity([0,0,0,_v],0);
         }
-        uint _ycrv = IERC20(ycrv).balanceOf(address(this));
-        if (_ycrv > 0) {
-            IERC20(ycrv).safeApprove(yycrv, 0);
-            IERC20(ycrv).safeApprove(yycrv, _ycrv);
-            yERC20(yycrv).deposit(_ycrv);
+        uint _vcrv = IERC20(vcrv).balanceOf(address(this));
+        if (_vcrv > 0) {
+            IERC20(vcrv).safeApprove(vvcrv, 0);
+            IERC20(vcrv).safeApprove(vvcrv, _vcrv);
+            vERC20(vvcrv).deposit(_vcrv);
         }
     }
     
@@ -75,9 +75,9 @@ contract StrategyTUSDCurve {
     function withdraw(IERC20 _asset) external returns (uint balance) {
         require(msg.sender == controller, "!controller");
         require(want != address(_asset), "want");
-        require(y != address(_asset), "y");
-        require(ycrv != address(_asset), "ycrv");
-        require(yycrv != address(_asset), "yycrv");
+        require(v != address(_asset), "v");
+        require(vcrv != address(_asset), "vcrv");
+        require(vvcrv != address(_asset), "vvcrv");
         balance = _asset.balanceOf(address(this));
         _asset.safeTransfer(controller, balance);
     }
@@ -91,7 +91,7 @@ contract StrategyTUSDCurve {
             _amount = _amount.add(_balance);
         }
         
-        address _vault = IController(controller).vaults(address(want));
+        address _vault = EController(controller).vaults(address(want));
         require(_vault != address(0), "!vault"); // additional protection so we don't burn the funds
         IERC20(want).safeTransfer(_vault, _amount);
     }
@@ -104,59 +104,59 @@ contract StrategyTUSDCurve {
         
         balance = IERC20(want).balanceOf(address(this));
         
-        address _vault = IController(controller).vaults(address(want));
+        address _vault = EController(controller).vaults(address(want));
         require(_vault != address(0), "!vault"); // additional protection so we don't burn the funds
         IERC20(want).safeTransfer(_vault, balance);
     }
     
     function withdrawTUSD(uint256 _amount) internal returns (uint) {
-        IERC20(ycrv).safeApprove(curve, 0);
-        IERC20(ycrv).safeApprove(curve, _amount);
+        IERC20(vcrv).safeApprove(curve, 0);
+        IERC20(vcrv).safeApprove(curve, _amount);
         ICurveFi(curve).remove_liquidity(_amount, [uint256(0),0,0,0]);
     
-        uint256 _ydai = IERC20(ydai).balanceOf(address(this));
-        uint256 _yusdc = IERC20(yusdc).balanceOf(address(this));
-        uint256 _yusdt = IERC20(yusdt).balanceOf(address(this));
+        uint256 _vdai = IERC20(vdai).balanceOf(address(this));
+        uint256 _vusdc = IERC20(vusdc).balanceOf(address(this));
+        uint256 _vusdt = IERC20(vusdt).balanceOf(address(this));
     
-        if (_ydai > 0) {
-            IERC20(ydai).safeApprove(curve, 0);
-            IERC20(ydai).safeApprove(curve, _ydai);
-            ICurveFi(curve).exchange(0, 3, _ydai, 0);
+        if (_vdai > 0) {
+            IERC20(vdai).safeApprove(curve, 0);
+            IERC20(vdai).safeApprove(curve, _vdai);
+            ICurveFi(curve).exchange(0, 3, _vdai, 0);
         }
-        if (_yusdc > 0) {
-            IERC20(yusdc).safeApprove(curve, 0);
-            IERC20(yusdc).safeApprove(curve, _yusdc);
-            ICurveFi(curve).exchange(1, 3, _yusdc, 0);
+        if (_vusdc > 0) {
+            IERC20(vusdc).safeApprove(curve, 0);
+            IERC20(vusdc).safeApprove(curve, _vusdc);
+            ICurveFi(curve).exchange(1, 3, _vusdc, 0);
         }
-        if (_yusdt > 0) {
-            IERC20(yusdt).safeApprove(curve, 0);
-            IERC20(yusdt).safeApprove(curve, _yusdt);
-            ICurveFi(curve).exchange(2, 3, _yusdt, 0);
+        if (_vusdt > 0) {
+            IERC20(vusdt).safeApprove(curve, 0);
+            IERC20(vusdt).safeApprove(curve, _vusdt);
+            ICurveFi(curve).exchange(2, 3, _vusdt, 0);
         }
         
         uint _before = IERC20(want).balanceOf(address(this));
-        yERC20(ytusd).withdraw(IERC20(ytusd).balanceOf(address(this)));
+        vERC20(vtusd).withdraw(IERC20(vtusd).balanceOf(address(this)));
         uint _after = IERC20(want).balanceOf(address(this));
         
         return _after.sub(_before);
     }
     
     function _withdrawAll() internal {
-        uint _yycrv = IERC20(yycrv).balanceOf(address(this));
-        if (_yycrv > 0) {
-            yERC20(yycrv).withdraw(_yycrv);
-            withdrawTUSD(IERC20(ycrv).balanceOf(address(this)));
+        uint _vvcrv = IERC20(vvcrv).balanceOf(address(this));
+        if (_vvcrv > 0) {
+            vERC20(vvcrv).withdraw(_vvcrv);
+            withdrawTUSD(IERC20(vcrv).balanceOf(address(this)));
         }
     }
     
     function _withdrawSome(uint256 _amount) internal returns (uint) {
-        // calculate amount of ycrv to withdraw for amount of _want_
-        uint _ycrv = _amount.mul(1e18).div(ICurveFi(curve).get_virtual_price());
-        // calculate amount of yycrv to withdraw for amount of _ycrv_
-        uint _yycrv = _ycrv.mul(1e18).div(yERC20(yycrv).getPricePerFullShare());
-        uint _before = IERC20(ycrv).balanceOf(address(this));
-        yERC20(yycrv).withdraw(_yycrv);
-        uint _after = IERC20(ycrv).balanceOf(address(this));
+        // calculate amount of vcrv to withdraw for amount of _want_
+        uint _vcrv = _amount.mul(1e18).div(ICurveFi(curve).get_virtual_price());
+        // calculate amount of vvcrv to withdraw for amount of _vcrv_
+        uint _vvcrv = _vcrv.mul(1e18).div(vERC20(vvcrv).getPricePerFullShare());
+        uint _before = IERC20(vcrv).balanceOf(address(this));
+        vERC20(vvcrv).withdraw(_vvcrv);
+        uint _after = IERC20(vcrv).balanceOf(address(this));
         return withdrawTUSD(_after.sub(_before));
     }
     
@@ -164,29 +164,29 @@ contract StrategyTUSDCurve {
         return IERC20(want).balanceOf(address(this));
     }
     
-    function balanceOfYYCRV() public view returns (uint) {
-        return IERC20(yycrv).balanceOf(address(this));
+    function balanceOfVVCRV() public view returns (uint) {
+        return IERC20(vvcrv).balanceOf(address(this));
     }
     
-    function balanceOfYYCRVinYCRV() public view returns (uint) {
-        return balanceOfYYCRV().mul(yERC20(yycrv).getPricePerFullShare()).div(1e18);
+    function balanceOfVVCRVinVCRV() public view returns (uint) {
+        return balanceOfVVCRV().mul(vERC20(vvcrv).getPricePerFullShare()).div(1e18);
     }
     
-    function balanceOfYYCRVinyTUSD() public view returns (uint) {
-        return balanceOfYYCRVinYCRV().mul(ICurveFi(curve).get_virtual_price()).div(1e18);
+    function balanceOfVVCRVinvTUSD() public view returns (uint) {
+        return balanceOfVVCRVinVCRV().mul(ICurveFi(curve).get_virtual_price()).div(1e18);
     }
     
-    function balanceOfYCRV() public view returns (uint) {
+    function balanceOfVCRV() public view returns (uint) {
         return IERC20(ycrv).balanceOf(address(this));
     }
     
-    function balanceOfYCRVyTUSD() public view returns (uint) {
-        return balanceOfYCRV().mul(ICurveFi(curve).get_virtual_price()).div(1e18);
+    function balanceOfVCRVvTUSD() public view returns (uint) {
+        return balanceOfVCRV().mul(ICurveFi(curve).get_virtual_price()).div(1e18);
     }
     
     function balanceOf() public view returns (uint) {
         return balanceOfWant()
-               .add(balanceOfYYCRVinyTUSD());
+               .add(balanceOfVVCRVinvTUSD());
     }
     
     function setGovernance(address _governance) external {
